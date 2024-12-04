68454fd4fdff812569fe97689b6f2510c5facb01
NND <11896364+nndvn@users.noreply.github.com>
2024-12-05 02:34:02 +0700

    Update run_once_after_configure-defaults.sh

--- .chezmoiscripts/darwin/run_once_after_configure-defaults.sh
+++ .chezmoiscripts/darwin/run_once_after_configure-defaults.sh
@@ -23,7 +23,7 @@ defaults write com.apple.dock magnification -bool true
 # defaults write com.apple.dock largesize -int 128
 defaults write com.apple.dock mineffect -string "genie"
 # defaults write com.apple.dock show-recents -bool false
-defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide -bool false
 defaults write com.apple.dock autohide-delay -float 0
 # defaults write com.apple.dock autohide-time-modifier -float 2
 # defaults write com.apple.dock persistent-apps -array # wipe all default app icons
