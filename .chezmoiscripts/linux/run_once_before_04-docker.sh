366ef0b64763ab5a320af2fc3110c418723044ff
NND <d@nnd.vn>
2024-12-08 05:15:57 +0700

    .chezmoiscripts/linux/run_once_before_04-docker.sh

--- .chezmoiscripts/linux/run_once_before_04-docker.sh
+++ .chezmoiscripts/linux/run_once_before_04-docker.sh
@@ -3,8 +3,14 @@
 set -eufo pipefail
 echo "run_once_before_04-docker.sh"
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh ./get-docker.sh --dry-run
-sudo sh ./get-docker.sh
-sudo groupadd docker
+if [ $(command -v docker) ]; then
+    echo "docker already installed: $(docker --version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')"
+else
+    echo "docker not found, installing docker"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    # sudo sh ./get-docker.sh --dry-run
+    sudo sh ./get-docker.sh
+fi
+
+# sudo groupadd docker
 sudo usermod -aG docker $USER
