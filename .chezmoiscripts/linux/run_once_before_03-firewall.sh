d4d3d46a281bec1bee795a13e5c827fa41d9c519
NND <d@nnd.vn>
2024-12-09 20:16:04 +0700

    Update .chezmoiscripts/linux/run_once_before_03-firewall.sh

--- .chezmoiscripts/linux/run_once_before_03-firewall.sh
+++ .chezmoiscripts/linux/run_once_before_03-firewall.sh
@@ -3,10 +3,17 @@
 set -eufo pipefail
 echo "run_once_before_03-firewall.sh"
 
-sudo ufw status
-sudo ufw enable
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow in on tailscale0
-sudo ufw limit in 22369/tcp
-sudo ufw reload
+# check if ufw is active
+ufw_status=$(sudo ufw status | grep -w "Status:")
+
+if [[ $ufw_status == *"Status: active"* ]]; then
+    echo "ufw is active"
+else
+    echo "ufw is inactive"
+    sudo ufw enable
+    sudo ufw default deny incoming
+    sudo ufw default allow outgoing
+    sudo ufw allow in on tailscale0
+    # sudo ufw limit in 22369/tcp
+    sudo ufw reload
+fi
