c5298dbf4aeec10d5578e3be2d1959418fad1e37
NND <d@nnd.vn>
2024-12-22 17:45:27 +0700

    WIP

--- .chezmoiscripts/darwin/run_once_after_configure-defaults.sh
+++ .chezmoiscripts/darwin/run_once_after_configure-defaults.sh
@@ -21,23 +21,24 @@ osascript -e 'tell application "System Preferences" to quit'
 
 # dock
 defaults write com.apple.dock tilesize -int 69
-defaults write com.apple.dock magnification -bool true
+# defaults write com.apple.dock magnification -bool true
 # defaults write com.apple.dock largesize -int 128
-defaults write com.apple.dock mineffect -string "genie"
+# defaults write com.apple.dock mineffect -string "genie"
+defaults write com.apple.dock "mineffect" -string "suck"
 defaults write com.apple.dock show-recents -bool false
-defaults write com.apple.dock autohide -bool false
-defaults write com.apple.dock autohide-delay -float 0
+# defaults write com.apple.dock autohide -bool false
+# defaults write com.apple.dock autohide-delay -float 0
 # defaults write com.apple.dock autohide-time-modifier -float 2
-defaults write com.apple.dock persistent-apps -array # wipe all default app icons
+# defaults write com.apple.dock persistent-apps -array # wipe all default app icons
 killall Dock
 
 # finder
 defaults write com.apple.finder AppleShowAllFiles -bool true
 # defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+# defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 # defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 # defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+# defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 killall Finder
 
 # done
