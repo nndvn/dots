955a63d84b306246f6d01ca34185759c57ea24f6
NND <11896364+nndvn@users.noreply.github.com>
2024-12-25 02:57:44 +0700

    Create run_onchange_after_01-install-packages.sh

---
+++ .chezmoiscripts/darwin/run_onchange_after_01-install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eufo pipefail
+echo "run_onchange_after_01-install-packages.sh"
+
+echo "installing packages"
+# check if .brewfile exists
+if [ -f .brewfile ]; then
+    # install packages using brew bundle
+    # brew bundle install
+    # brew bundle install --no-lock > /dev/null
+    brew bundle install --no-lock --file=~/.brewfile
+else
+    echo "no .brewfile found"
+fi
